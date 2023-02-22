fbfbfb901f35a07d053bdb65b1d4328fe7cddfd5
Jeremiah <jeremiah.d.wise@gmail.com>
2023-02-22 12:07:11 -0600

    ll append and pop

--- DataStructures/LinkedList/EXERCISE-LL-Pop.py
+++ DataStructures/LinkedList/EXERCISE-LL-Pop.py
@@ -11,9 +11,15 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
     def append(self, value):
         new_node = Node(value)
-        if self.head is None:
+        if self.length == 0:
             self.head = new_node
             self.tail = new_node
         else:
@@ -22,16 +28,7 @@ class LinkedList:
         self.length += 1
         return True
 
-    def prepend(self, value):
-        new_node = Node(value)
-
-    def insert(self, index, value):
-        # create new Node
-        # insert Node
-        pass
-
     def pop(self):
-
         if self.length == 0:
             return None
         temp = self.head
@@ -47,8 +44,23 @@ class LinkedList:
             self.tail == None
         return temp
 
-    def print_list(self):
-        temp = self.head
-        while temp is not None:
-            print(temp.value)
-            temp = temp.next
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+
+# (2) Items - Returns 2 Node
+print(my_linked_list.pop().value)
+# (1) Item -  Returns 1 Node
+print(my_linked_list.pop().value)
+# (0) Items - Returns None
+print(my_linked_list.pop())
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    2
+    1
+    None
+
+"""
